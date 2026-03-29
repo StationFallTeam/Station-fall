119cbe5e3bf2067508c4e903ab2719df6262aec4
Zac-Evans62 <pvr55w2d67@privaterelay.appleid.com>
2026-03-29 13:00:15 -0400

    attempted to fix issue

--- src/test_cases/test_camera.py
+++ src/test_cases/test_camera.py
@@ -1,5 +1,6 @@
+import sys
 import os
-os.environ["SDL_VIDEODRIVER"] = "dummy"
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from unittest.mock import Mock
