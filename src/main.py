1a82dcbe1a7a3fc0a117cfce2386c333d71d7f66
Meheraj Khatri <mkhatri5@kent.edu>
2026-03-30 14:33:29 -0400

    Updates

--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ import math
 import sys
 import math
 
-from .player import Player
-from .enemy import Enemy
-from .render import draw_objects, apply_brightness, draw_credits, render_menu_screen, render_inventory_screen, render_game_over_screen
-from .camera import Camera          # Added for camera - Meheraj
-from .background import SpaceBackground # Added for parallax background - Meheraj
-from .world import World
-from .coin import Coin
-from .inventory_ui import InventoryUI
-from .floating_texts import FloatingText  # Added for floating damage text - Meheraj
+from player import Player
+from enemy import Enemy
+from render import draw_objects, apply_brightness, draw_credits, render_menu_screen, render_inventory_screen, render_game_over_screen
+from camera import Camera          # Added for camera - Meheraj
+from background import SpaceBackground # Added for parallax background - Meheraj
+from world import World
+from coin import Coin
+from inventory_ui import InventoryUI
+from floating_texts import FloatingText  # Added for floating damage text - Meheraj
 
 # NOTE:
 # When debugging pygbag web crashes, temporarily wrap main() in a try/except
